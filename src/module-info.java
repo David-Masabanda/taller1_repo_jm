86a0a13e4ac55b6d7cd0bd341df5317357562c28
josue <josue@MSI>
2022-05-19 10:43:27 -0500

    Archivos necesarios para el proyecto

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module taller1_repo_jm_practica {
+}
---
+++ src/taller1_repo_jm/Main.java
@@ -0,0 +1,9 @@
+package taller1_repo_jm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Mi primer proyecto subido a GitHub");
+	}
+
+}
